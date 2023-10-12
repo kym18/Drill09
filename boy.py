f7ea02b8ad6a51b4d5d224ce7b59c587fd6b1981
kym18 <cooiyoo@naver.com>
2023-10-12 22:26:38 +0900

    sleep 상태에서 a키 모션 추가

--- boy.py
+++ boy.py
@@ -163,7 +163,7 @@ class StateMachine:
         self.transitions = {
             Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep, a_down: AutoRun},
             Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
-            Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle},
+            Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle, a_down: AutoRun},
             AutoRun: {time_out:Idle,right_down:Run,left_down:Run,right_up:Run,left_up:Run}
         }
         pass
